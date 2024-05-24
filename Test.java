f2ae7a55d77e3f560745a5aa1122b0863af79c5e
elysia <3252765235@qq.com>
2024-05-24 19:12:45 +0800

    测试（第二次）

--- Test.java
+++ Test.java
@@ -7,6 +7,6 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("测试同时推送到gitee和github");
+        System.out.println("测试同时推送到gitee和github？");
     }
 }
