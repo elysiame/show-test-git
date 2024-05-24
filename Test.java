516d31510bf9ecc1e08ba76a10d5b5616b4cad0e
elysia <3252765235@qq.com>
2024-05-24 19:08:06 +0800

    测试（第一次）

---
+++ Test.java
@@ -0,0 +1,12 @@
+/**
+ * projectName:PACKAGE_NAME
+ *
+ * @author 飞
+ * @create by 2024/5/2419:06
+ * description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试同时推送到gitee和github");
+    }
+}
